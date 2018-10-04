01cf7db3ed423c618ddc47e9ac150f96b48d8656
ritabc <ritabc@gmail.com>
2018-10-03 23:55:06 -0700

    switch to position:absolute in one move span, with x and y coords

--- src/app/visualize/visualize.component.ts
+++ src/app/visualize/visualize.component.ts
@@ -15,9 +15,12 @@ export class VisualizeComponent implements OnInit {
     '--ravenOneColor': '#1f1f1f',
     '--ravenTwoColor': '#9f9f9f',
   }
+
   constructor() { }
 
   ngOnInit() {
+    // document.querySelector(".B2")[0].style.display = 'none';
+
   }
 
   public doStuff() {
@@ -27,21 +30,33 @@ export class VisualizeComponent implements OnInit {
     /// display 6D as is, (don't forget to select all 6D's)
     /// display others as appropriate
     //// raven-one in D3
-    this.setImproper()
-
 
 /// give up on host, dynamically set which circle should be raven by passing a variable into the css
   }
 
-  public setClasses() {
-    let styles = {}
+  public setStyles() {
+    let styles
 
     if ($('div').hasClass("B2")) {
       styles = {
         'fill':'black'
       };
     }
+
     return styles
   }
 
 }
+// first, hide all circles
+// then, display the 4
+//
+
+// move, display position with svg positions
+// create one dark brown circle with [ngStyle]="lark-one-var"
+// method setImproper() will give that lark the appropriate x, y values
+
+// with grid systems, A1, ... D9
+// give each one [ngStyle]='A1', etc
+// if we want B2 to display as lark one, set variable B2 = { style object }
+// how to select the rest to hide?
+// give [ngClass]=tohide variable to each circle,
